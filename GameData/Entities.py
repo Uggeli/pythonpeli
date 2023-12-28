1490e427ff4e1c5b49e31b43c8b18a5761efab2b
Uggeli <sakkisen.ville@gmail.com>
2023-12-28 16:22:40 +0200

    Add Entity class to manage game entities

---
+++ GameData/Entities.py
@@ -0,0 +1,29 @@
+class Entity:
+    position = None
+    name = None
+    session_id = None
+    color = None
+
+    def __init__(self, name, position, color):
+        self.name = name
+        self.position = position
+        self.color = color
+
+    def move(self, x, y):
+        self.position = (x, y)
+
+    def __str__(self):
+        return f"{self.name} at {self.position}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "position": self.position,
+            "color": self.color
+        }
