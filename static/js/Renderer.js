5f170435ac4261c06565b68dcd2aa4bba9503663
Uggeli <sakkisen.ville@gmail.com>
2023-12-28 16:24:25 +0200

    Add Renderer class for rendering the game map

---
+++ static/js/Renderer.js
@@ -0,0 +1,44 @@
+// Renderer.js
+class Renderer {
+    constructor() {
+        this.canvas = null;
+        this.ctx = null;
+        this.cellSize = 30; // Set cell size
+    }
+
+    setup() {
+        this.canvas = document.createElement('canvas');
+        this.canvas.id = 'GameCanvas';
+        this.canvas.width = 300; // Example size
+        this.canvas.height = 300;
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    renderMap(gameState) {
+        if (!gameState || !gameState.tiles) {
+            return;
+        }
+        // Clear canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // Render each cell of the map
+        for (let y = 0; y < gameState.tiles.length; y++) {
+            for (let x = 0; x < gameState.tiles[y].length; x++) {
+                const cell = gameState.tiles[y][x];
+                if (cell === 0) {
+                    // Empty cell
+                    this.ctx.fillStyle = 'white';
+                    this.ctx.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+                } else if (cell === 'w') {
+                    // Wall
+                    this.ctx.fillStyle = 'black';
+                    this.ctx.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+                } else {
+                    // Player
+                    this.ctx.fillStyle = cell.color;
+                    this.ctx.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+                }
+            }
+        }
+    }
+}
