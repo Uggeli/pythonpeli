4981f74f885d01b477af9ebda388217c49e481c3
Uggeli <sakkisen.ville@gmail.com>
2023-12-28 16:22:26 +0200

    Add Action class to GameData

---
+++ GameData/Action.py
@@ -0,0 +1,8 @@
+class Action:
+    # action, actor, target, data
+    def __init__(self, action, actor, target, data):
+        self.action = action
+        self.actor = actor
+        self.target = target
+        self.data = data
+
