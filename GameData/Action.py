4da6d6612fbc347f45d8593cfb3d80c36e6aeeee
Uggeli <sakkisen.ville@gmail.com>
2023-12-30 10:15:20 +0200

    Reorder parameters in Action class constructor

--- GameData/Action.py
+++ GameData/Action.py
@@ -1,6 +1,6 @@
 class Action:
     # action, actor, target, data
-    def __init__(self, action, actor, target, data):
+    def __init__(self, actor, action, target, data):
         self.action = action
         self.actor = actor
         self.target = target
