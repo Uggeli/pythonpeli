7f7b8f70924c69cfbd847963e2b258c636ee9489
Uggeli <sakkisen.ville@gmail.com>
2024-01-03 22:47:59 +0200

    Update import statements in __init__.py

--- GameData/__init__.py
+++ GameData/__init__.py
@@ -1,5 +1,5 @@
 from .Entities import Entity  # noqa: E402, F401
-from .Managers import MovementManager, CollisionManager  # noqa: E402, F401
+from .Managers import TextureManager, ActionManager, MapManager  # noqa: E402, F401
 from .EventHub import Event, EventHub  # noqa: E402, F401
 from .GameEngine import GameEngine  # noqa: E402, F401
 from .Action import Action  # noqa: E402, F401
