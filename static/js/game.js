6634239fd413fb073c00bd928a6d4c93b49e19d0
Uggeli <sakkisen.ville@gmail.com>
2024-01-04 11:32:54 +0200

    Update Connection setup and add TextureManager

--- static/js/game.js
+++ static/js/game.js
@@ -2,10 +2,11 @@
 
 let gameState = null; // The current game state
 const renderer = new Renderer();
-const connection = new Connection(renderer);
+const connection = new Connection();
 const inputManager = new InputManager();
+const textureManager = new TextureManager();
 
 inputManager.listen();
 renderer.setup();
-connection.setup();
+connection.connect();
 
