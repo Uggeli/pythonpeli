15c440e62debbf0d84a3efffb61e7f421195c8c6
Uggeli <sakkisen.ville@gmail.com>
2024-01-04 11:33:12 +0200

    Refactor Renderer.js: Remove unused code and improve texture handling

--- static/js/Renderer.js
+++ static/js/Renderer.js
@@ -1,7 +1,5 @@
 // Renderer.js
 class Renderer {
-    Textures = {}
-
     constructor() {
         this.canvas = null;
         this.ctx = null;
@@ -11,21 +9,10 @@ class Renderer {
     setup() {
         this.canvas = document.createElement('canvas');
         this.canvas.id = 'GameCanvas';
-        this.canvas.width = 300; // Example size
-        this.canvas.height = 300;
+        this.canvas.width = 800; 
+        this.canvas.height = 800;
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
-    }
-
-    addTexture(texture) {
-        if (this.Textures[texture.name]) {
-            return;
-        }
-        this.Textures[texture.name] = texture;
-    }
-
-    getTexture(name) {
-        return this.Textures[name];
     }
 
     renderMap(gameState) {
@@ -35,17 +22,26 @@ class Renderer {
         // Clear canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         // Render each cell of the map
+        let missingTextures = [];
         for (let y = 0; y < gameState.tiles.length; y++) {
             for (let x = 0; x < gameState.tiles[y].length; x++) {
                 const cell = gameState.tiles[y][x];
-                const texture = this.getTexture(cell);
-                if (!texture) {
-                    connection.handleRequestTexture(cell);
-                }
-                else {
-                    this.ctx.drawImage(texture, x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+                const textureObj = textureManager.getTexture(cell);
+                console.log(textureManager)
+                console.log(textureObj)
+                if (!textureObj) {
+                    if (!missingTextures.includes(cell)) {
+                        missingTextures.push(cell);
+                    }
+                    continue;
+                } else {
+                    this.ctx.drawImage(textureObj.texture, x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
                 }
             }
+        }
+        if (missingTextures.length > 0) {
+            console.warn(`Missing textures: ${missingTextures.join(', ')}`);
+            textureManager.requestTextures(missingTextures);
         }
     }
 }
